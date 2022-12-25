499693bec8be911620753cc74118786f6eb86924
David Giwa <david.s.giwa@gmail.com>
2022-12-25 00:30:45 -0600

    Newer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const server = express();
 const { client } = require('./db');
 client.connect();
+const apiRouter = require('./api');
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
@@ -15,19 +16,52 @@ server.use(morgan('dev'));
 
 server.use(express.json())
 
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
   
-    next();
-  });
+  next();
+});
 
-  server.get('/add/:first/to/:second', (req, res, next) => {
-    res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
-      Number(req.params.first) + Number(req.params.second)
-     }</h1>`);
-  });
+server.get('/add/:first/to/:second', (req, res, next) => {
+  res.send(`
+  
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Juicebox API</title>
+  </head>
+  <body>
+  <h1>JuiceBoxSunsetKiss</h1>
+  <h2>AnimeEnding</h2>
+  <h3>/users</h3>
+  <ul>
+  <li>GET /</li>
+  <li>POST /register</li>
+  <li>POST /login</li>
+  <li>PATCH /:userId</li>
+  <li>DELETE /:userId</li>
+  </ul>
+  <h3>/Posts</h3>
+  <ul>
+  <li>GET /</li>
+  <li>POST /</li>
+  <li>PATCH /:postId</li>
+  <li>DELETE /:postId</li>
+  </ul>
+  <h3>/Tags</h3>
+  <ul>
+  <li>GET /</li>
+  <li>GET /:tagName/posts</li>
+  </ul>
+  <h2>Summer Days...Drifting away...</h2>
+  </body>
+  </html>
+  `)
+})
+server.use('/api', apiRouter);
