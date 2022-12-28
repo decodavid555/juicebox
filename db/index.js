c97ccf531bac1ae1629d389a527ec28751f79766
David Giwa <david.s.giwa@gmail.com>
2022-12-28 17:20:01 -0600

    errors

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 /************************SLOWLY REALIZING NOTHING WORKS IF ITS NOT IN ORDER -_- */
 
 const { Client } = require("pg");
-
-const client = new Client("postgres://localhost:5432/juicebox-dev");
+const client = new Client (process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 //**************** User DudeBros ********************
 
--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 require('dotenv').config();
 
-const PORT = 3000;
+const {PORT = 3000} = process.env;
 const express = require('express');
-const server = express();
+const morgan = require('morgan');
+const apiRouter = require('./api');
 const { client } = require('./db');
 client.connect();
-const apiRouter = require('./api');
 
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
-});
+const server = express();
 
-
-const morgan = require('morgan');
 server.use(morgan('dev'));
 
-server.use(express.json())
+server.use(express.json());
 
 
 server.use((req, res, next) => {
@@ -27,42 +23,13 @@ server.use((req, res, next) => {
 });
 
 server.get('/add/:first/to/:second', (req, res, next) => {
-  res.send(`
-  
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Juicebox API</title>
-  </head>
-  <body>
-  <h1>JuiceBoxSunsetKiss</h1>
-  <h2>AnimeEnding</h2>
-  <h3>/users</h3>
-  <ul>
-  <li>GET /</li>
-  <li>POST /register</li>
-  <li>POST /login</li>
-  <li>PATCH /:userId</li>
-  <li>DELETE /:userId</li>
-  </ul>
-  <h3>/Posts</h3>
-  <ul>
-  <li>GET /</li>
-  <li>POST /</li>
-  <li>PATCH /:postId</li>
-  <li>DELETE /:postId</li>
-  </ul>
-  <h3>/Tags</h3>
-  <ul>
-  <li>GET /</li>
-  <li>GET /:tagName/posts</li>
-  </ul>
-  <h2>Summer Days...Drifting away...</h2>
-  </body>
-  </html>
-  `)
-})
+  res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+    Number(req.params.first) + Number(req.params.second)
+   }</h1>`);
+});
+
 server.use('/api', apiRouter);
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
